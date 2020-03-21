cac4ed33497d4a0f0bbdff30ff7b89953104274b
dernasherbrezon <rodionovamp@mail.ru>
2020-03-21 20:21:44 +0000

    add sample registers

--- src/EpeverTracer.c
+++ src/EpeverTracer.c
@@ -6,8 +6,28 @@
 #include <string.h>
 
 typedef struct {
-	modbus_t *modbus;
-} epever_tracer_t;
+	char *category;
+	char *parameter;
+	int addr;
+	int size;
+} epever_register_t;
+
+const epever_register_t registers[] = {
+		{"solar", "voltage", 0x3100, 1},
+		{"solar", "current", 0x3101, 1},
+		{"solar", "power", 0x3102, 2},
+		{"battery", "voltage", 0x3104, 1},
+		{"battery", "current", 0x3105, 1},
+		{"battery", "power", 0x3106, 2},
+		{"battery", "temperature", 0x3110, 1},
+		{"battery", "remoteTemperature", 0x311B, 1},
+		{"battery", "realRatedVoltage", 0x311D, 1},
+		{"battery", "soc", 0x311A, 1},
+		{"load", "voltage", 0x310C, 1},
+		{"load", "current", 0x310D, 1},
+		{"load", "power", 0x310E, 2},
+		{"device", "temperature", 0x3112, 1}
+};
 
 int epever_tracer_process(char *device, uint32_t timeoutSecond, int verbose) {
 	// values are defined by the protocol
@@ -49,12 +69,23 @@ int epever_tracer_process(char *device, uint32_t timeoutSecond, int verbose) {
 		return EXIT_FAILURE;
 	}
 	uint16_t *output = (uint16_t *) malloc(2 * sizeof(uint16_t));
-	memset(output, 0, 2 * sizeof(uint16_t));
-	rc = modbus_read_input_registers(modbus, 0x3004, 0x1, output);
-	if (rc == -1) {
-		fprintf(stderr, "Failed to modbus_read_input_registers: %s\n", modbus_strerror(errno));
+
+	epever_register_t *ptr = registers;
+	const epever_register_t *endPtr = registers + sizeof(registers) / sizeof(registers[0]);
+	while (ptr < endPtr) {
+		memset(output, 0, 2 * sizeof(uint16_t));
+		for (int i = 0; i < ptr->size; i++) {
+			uint16_t dest;
+			rc = modbus_read_input_registers(modbus, ptr->addr + i, 0x1, &dest);
+			if (rc == -1) {
+				fprintf(stderr, "Failed to modbus_read_input_registers: %s\n", modbus_strerror(errno));
+			}
+			output[i] = dest;
+		}
+		uint32_t fullRegister = (output[1] << 8) | (output[0]);
+		fprintf(stdout, "%s %s=%f\n", ptr->category, ptr->parameter, (float) (fullRegister / 100.0));
+		ptr++;
 	}
-	fprintf(stdout, "voltage=%f\n", (float)(output[0] / 100.0));
 	free(output);
 	modbus_close(modbus);
 	modbus_free(modbus);
